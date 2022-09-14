ba90dae01634a3284639d09a020634275f5593cd
zzzmix <112614277+zzzmix@users.noreply.github.com>
2022-09-15 01:07:28 +0900

    change text

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'Hello World　！！ It works!\n',
+    var message = 'Hello World　, It works!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
