07892b4a5438bb696b63c9bcc421096c69f18286
zzzmix <112614277+zzzmix@users.noreply.github.com>
2022-09-15 17:12:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = 'NodeJS Hello World. It works!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
+        apikey =  process.env.API_KEY ?? 'api key',
+        response = [message, version, apikey].join('\n');
     res.end(response);
 });
 server.listen();
